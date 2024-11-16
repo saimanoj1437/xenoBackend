1be5398d03afd9f4fa38e01d37a9e9d48259cb87
Manoj Bhaskar <130078620+saimanoj1437@users.noreply.github.com>
2024-11-16 22:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,13 +6,12 @@ const MongoStore = require('connect-mongo');
 const orderRoutes = require('./routes/order');
 const authRoutes = require('./routes/authRoutes'); // Import auth routes
 const audienceRoutes = require('./routes/customer'); // Import audience routes
-require('dotenv').config();
 
 const app = express();
 
 // Use CORS middleware
 app.use(cors({
-    origin: 'https://xeno-frontend-liard.vercel.app/',  // Allow requests from frontend
+    origin: 'http://localhost:3000',  // Allow requests from frontend
     methods: 'GET, POST, PUT, DELETE',
     credentials: true
 }));
@@ -22,10 +20,12 @@ app.use(express.json()); // Parse JSON requests
 
 // Set up session middleware
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: '92fc674065e2f0ac4dd582b888364352800f5343a52f5993f736db1ee62c94fc1d7fc0a7b8c28eebfa6130a7b06496802d3f0064d80e51fdcacb56ee6502427b',
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://manojbhaskarmvs2021:qPbo5rS3oRRweiwe@xenobackend.ld71e.mongodb.net/?retryWrites=true&w=majority&appName=xenoBackend'
+    }),
     cookie: {
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
@@ -34,7 +34,10 @@ app.use(session({
 }));
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://manojbhaskarmvs2021:qPbo5rS3oRRweiwe@xenobackend.ld71e.mongodb.net/?retryWrites=true&w=majority&appName=xenoBackend', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://manojbhaskarmvs2021:qPbo5rS3oRRweiwe@xenobackend.ld71e.mongodb.net/?retryWrites=true&w=majority&appName=xenoBackend', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 mongoose.connection.on('connected', () => console.log('MongoDB connected successfully.'));
 mongoose.connection.on('error', (err) => console.log('MongoDB connection error:', err));
 mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected.'));
@@ -45,5 +48,5 @@ app.use('/api/auth', authRoutes); // Add auth routes
 app.use('/api/audience', audienceRoutes); // Add audience routes
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
