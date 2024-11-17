4df2c99dda589ce206d4fa1f9ee4948f13859ca5
Manoj Bhaskar <130078620+saimanoj1437@users.noreply.github.com>
2024-11-17 22:14:27 +0530

    Update customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -1,25 +1,24 @@
-// routes/audienceRoutes.js
 const express = require('express');
-const Audience = require('../models/Audience'); // Import Audience model
+const Audience = require('../models/Audience'); 
 const router = express.Router();
 
-// POST route to add an audience
+
 router.post('/add', async (req, res) => {
     try {
-        const audience = new Audience(req.body); // Create a new Audience instance with data from request body
-        await audience.save(); // Save the audience data to MongoDB
-        res.status(201).send(audience); // Respond with the saved audience data
+        const audience = new Audience(req.body); 
+        await audience.save(); 
+        res.status(201).send(audience); 
     } catch (error) {
-        res.status(400).send(error); // Handle errors by sending a 400 status code
+        res.status(400).send(error); 
     }
 });
 
 router.get('/', async (req, res) => {
     try {
-        const audiences = await Audience.find(); // Fetch all audiences from MongoDB
-        res.status(200).send(audiences); // Respond with the list of audiences
+        const audiences = await Audience.find(); 
+        res.status(200).send(audiences); 
     } catch (error) {
-        res.status(500).send(error); // Handle errors by sending a 500 status code
+        res.status(500).send(error);
     }
 });
 
